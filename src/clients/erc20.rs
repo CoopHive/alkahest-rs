568ccb36b80c4ba7370104d61bf14a6c5e0f400e
疒奀 <yz@mmon.co>
2025-01-05 19:52:10 +0800

    erc20 relevant addresses

--- src/clients/erc20.rs
+++ src/clients/erc20.rs
@@ -1,1 +1,47 @@
-pub struct Erc20Client;
+use alloy::primitives::{address, Address};
+
+use crate::{
+    types::{PublicProvider, WalletProvider},
+    utils,
+};
+
+pub struct Erc20Addresses {
+    barter_utils: Address,
+    escrow_obligation: Address,
+    payment_obligation: Address,
+}
+
+pub struct Erc20Client {
+    wallet_provider: WalletProvider,
+    public_provider: PublicProvider,
+
+    addresses: Erc20Addresses,
+}
+
+impl Default for Erc20Addresses {
+    fn default() -> Self {
+        Self {
+            barter_utils: address!("3A40F65D2589a43Dc057bf820D8626F87D95307c"),
+            escrow_obligation: address!("248cd93922eBDf962c9ea10286E6566C75081948"),
+            payment_obligation: address!("702fab66515b3313dFd41E7CE70C2aF0033E2356"),
+        }
+    }
+}
+
+impl Erc20Client {
+    pub fn new(
+        private_key: impl ToString,
+        rpc_url: impl ToString + Clone,
+        addresses: Option<Erc20Addresses>,
+    ) -> eyre::Result<Self> {
+        let wallet_provider = utils::get_wallet_provider(private_key, rpc_url.clone())?;
+        let public_provider = utils::get_public_provider(rpc_url)?;
+
+        Ok(Erc20Client {
+            wallet_provider,
+            public_provider,
+
+            addresses: addresses.unwrap_or_default(),
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,18 @@ pub struct AlkahestClient {
 }
 
 impl AlkahestClient {
-    pub fn new(private_key: impl ToString, rpc_url: impl ToString + Clone) -> eyre::Result<Self> {
-        let wallet_provider = utils::get_wallet_provider(private_key, rpc_url.clone())?;
-        let public_provider = utils::get_public_provider(rpc_url)?;
+    pub fn new(
+        private_key: impl ToString + Clone,
+        rpc_url: impl ToString + Clone,
+    ) -> eyre::Result<Self> {
+        let wallet_provider = utils::get_wallet_provider(private_key.clone(), rpc_url.clone())?;
+        let public_provider = utils::get_public_provider(rpc_url.clone())?;
 
         Ok(AlkahestClient {
-            wallet_provider,
-            public_provider,
+            wallet_provider: wallet_provider.clone(),
+            public_provider: public_provider.clone(),
 
-            erc20: Erc20Client,
+            erc20: Erc20Client::new(private_key.clone(), rpc_url.clone(), None)?,
             erc721: Erc721Client,
             erc1155: Erc1155Client,
             token_bundle: TokenBundleClient,
