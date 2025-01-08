24223ed50019d9375d779f95ac0a5c8077c7aea1
疒奀 <yz@mmon.co>
2025-01-08 22:11:37 +0800

    1155, 721

--- src/clients/erc1155.rs
+++ src/clients/erc1155.rs
@@ -1,1 +1,93 @@
-pub struct Erc1155Client;
+use alloy::primitives::{address, Address, FixedBytes};
+use alloy::rpc::types::TransactionReceipt;
+use alloy::signers::local::PrivateKeySigner;
+
+use crate::contracts::{self};
+use crate::types::Erc1155Data;
+use crate::{types::WalletProvider, utils};
+
+pub struct Erc1155Addresses {
+    eas: Address,
+    barter_utils: Address,
+    escrow_obligation: Address,
+    payment_obligation: Address,
+}
+
+pub struct Erc1155Client {
+    signer: PrivateKeySigner,
+    wallet_provider: WalletProvider,
+
+    addresses: Erc1155Addresses,
+}
+
+impl Default for Erc1155Addresses {
+    fn default() -> Self {
+        Self {
+            eas: address!("4200000000000000000000000000000000000021"),
+            barter_utils: address!("3A40F65D2589a43Dc057bf820D8626F87D95307c"),
+            escrow_obligation: address!("248cd93922eBDf962c9ea10286E6566C75081948"),
+            payment_obligation: address!("702fab66515b3313dFd41E7CE70C2aF0033E2356"),
+        }
+    }
+}
+
+impl Erc1155Client {
+    pub fn new(
+        private_key: impl ToString + Clone,
+        rpc_url: impl ToString + Clone,
+        addresses: Option<Erc1155Addresses>,
+    ) -> eyre::Result<Self> {
+        let wallet_provider = utils::get_wallet_provider(private_key.clone(), rpc_url.clone())?;
+
+        Ok(Erc1155Client {
+            signer: private_key.to_string().parse()?,
+            wallet_provider,
+
+            addresses: addresses.unwrap_or_default(),
+        })
+    }
+
+    pub async fn buy_erc_1155_for_erc_1155(
+        &self,
+        bid: Erc1155Data,
+        ask: Erc1155Data,
+        expiration: u64,
+    ) -> eyre::Result<TransactionReceipt> {
+        let barter_utils_contract =
+            contracts::ERC1155BarterUtils::new(self.addresses.barter_utils, &self.wallet_provider);
+
+        let receipt = barter_utils_contract
+            .buyErc1155ForErc1155(
+                bid.address,
+                bid.id,
+                bid.value,
+                ask.address,
+                ask.id,
+                ask.value,
+                expiration,
+            )
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
+    }
+
+    pub async fn pay_erc_721_for_erc_721(
+        &self,
+        buy_attestation: FixedBytes<32>,
+    ) -> eyre::Result<TransactionReceipt> {
+        let barter_utils_contract =
+            contracts::ERC1155BarterUtils::new(self.addresses.barter_utils, &self.wallet_provider);
+
+        let receipt = barter_utils_contract
+            .payErc1155ForErc1155(buy_attestation)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
+    }
+}
--- src/clients/erc721.rs
+++ src/clients/erc721.rs
@@ -1,1 +1,85 @@
-pub struct Erc721Client;
+use alloy::primitives::{address, Address, FixedBytes};
+use alloy::rpc::types::TransactionReceipt;
+use alloy::signers::local::PrivateKeySigner;
+
+use crate::contracts::{self};
+use crate::types::Erc721Data;
+use crate::{types::WalletProvider, utils};
+
+pub struct Erc721Addresses {
+    eas: Address,
+    barter_utils: Address,
+    escrow_obligation: Address,
+    payment_obligation: Address,
+}
+
+pub struct Erc721Client {
+    signer: PrivateKeySigner,
+    wallet_provider: WalletProvider,
+
+    addresses: Erc721Addresses,
+}
+
+impl Default for Erc721Addresses {
+    fn default() -> Self {
+        Self {
+            eas: address!("4200000000000000000000000000000000000021"),
+            barter_utils: address!("3A40F65D2589a43Dc057bf820D8626F87D95307c"),
+            escrow_obligation: address!("248cd93922eBDf962c9ea10286E6566C75081948"),
+            payment_obligation: address!("702fab66515b3313dFd41E7CE70C2aF0033E2356"),
+        }
+    }
+}
+
+impl Erc721Client {
+    pub fn new(
+        private_key: impl ToString + Clone,
+        rpc_url: impl ToString + Clone,
+        addresses: Option<Erc721Addresses>,
+    ) -> eyre::Result<Self> {
+        let wallet_provider = utils::get_wallet_provider(private_key.clone(), rpc_url.clone())?;
+
+        Ok(Erc721Client {
+            signer: private_key.to_string().parse()?,
+            wallet_provider,
+
+            addresses: addresses.unwrap_or_default(),
+        })
+    }
+
+    pub async fn buy_erc_721_for_erc_721(
+        &self,
+        bid: Erc721Data,
+        ask: Erc721Data,
+        expiration: u64,
+    ) -> eyre::Result<TransactionReceipt> {
+        let barter_utils_contract =
+            contracts::ERC721BarterUtils::new(self.addresses.barter_utils, &self.wallet_provider);
+
+        let receipt = barter_utils_contract
+            .buyErc721ForErc721(bid.address, bid.id, ask.address, ask.id, expiration)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
+    }
+
+    pub async fn pay_erc_721_for_erc_721(
+        &self,
+        buy_attestation: FixedBytes<32>,
+    ) -> eyre::Result<TransactionReceipt> {
+        let barter_utils_contract =
+            contracts::ERC721BarterUtils::new(self.addresses.barter_utils, &self.wallet_provider);
+
+        let receipt = barter_utils_contract
+            .payErc721ForErc721(buy_attestation)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
+    }
+}
