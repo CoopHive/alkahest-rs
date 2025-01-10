b0b93356b48014c16e1ddef1f76755673b908386
疒奀 <yz@mmon.co>
2025-01-10 21:40:41 +0800

    simplify conversions with macro

--- src/types.rs
+++ src/types.rs
@@ -67,118 +67,84 @@ pub struct TokenBundleData {
     pub erc1155s: Vec<Erc1155Data>,
 }
 
-impl From<(TokenBundleData, Address)>
-    for contracts::token_bundle::TokenBundlePaymentObligation::StatementData
-{
-    fn from(data: (TokenBundleData, Address)) -> Self {
-        let (price, payee) = data;
-        contracts::token_bundle::TokenBundlePaymentObligation::StatementData {
-            erc20Tokens: price
-                .erc20s
-                .clone()
-                .into_iter()
-                .map(|erc20| erc20.address)
-                .collect(),
-            erc20Amounts: price.erc20s.into_iter().map(|erc20| erc20.value).collect(),
-            erc721Tokens: price
-                .erc721s
-                .clone()
-                .into_iter()
-                .map(|erc721| erc721.address)
-                .collect(),
-            erc721TokenIds: price.erc721s.into_iter().map(|erc721| erc721.id).collect(),
-            erc1155Tokens: price
-                .erc1155s
-                .clone()
-                .into_iter()
+impl TokenBundleData {
+    // Helper function to convert to the common token bundle format
+    fn into_bundle_components(
+        self,
+    ) -> (
+        Vec<Address>, // erc20Tokens
+        Vec<U256>,    // erc20Amounts
+        Vec<Address>, // erc721Tokens
+        Vec<U256>,    // erc721TokenIds
+        Vec<Address>, // erc1155Tokens
+        Vec<U256>,    // erc1155TokenIds
+        Vec<U256>,    // erc1155Amounts
+    ) {
+        (
+            self.erc20s.iter().map(|erc20| erc20.address).collect(),
+            self.erc20s.iter().map(|erc20| erc20.value).collect(),
+            self.erc721s.iter().map(|erc721| erc721.address).collect(),
+            self.erc721s.iter().map(|erc721| erc721.id).collect(),
+            self.erc1155s
+                .iter()
                 .map(|erc1155| erc1155.address)
                 .collect(),
-            erc1155TokenIds: price
-                .erc1155s
-                .clone()
-                .into_iter()
-                .map(|erc1155| erc1155.id)
-                .collect(),
-            erc1155Amounts: price
-                .erc1155s
-                .into_iter()
-                .map(|erc1155| erc1155.value)
-                .collect(),
-            payee,
-        }
+            self.erc1155s.iter().map(|erc1155| erc1155.id).collect(),
+            self.erc1155s.iter().map(|erc1155| erc1155.value).collect(),
+        )
     }
 }
 
-impl From<contracts::token_bundle::TokenBundlePaymentObligation::StatementData>
-    for contracts::erc20_barter_cross_token::TokenBundlePaymentObligation::StatementData
-{
-    fn from(data: contracts::token_bundle::TokenBundlePaymentObligation::StatementData) -> Self {
-        Self {
-            erc20Tokens: data.erc20Tokens,
-            erc20Amounts: data.erc20Amounts,
-            erc721Tokens: data.erc721Tokens,
-            erc721TokenIds: data.erc721TokenIds,
-            erc1155Tokens: data.erc1155Tokens,
-            erc1155TokenIds: data.erc1155TokenIds,
-            erc1155Amounts: data.erc1155Amounts,
-            payee: data.payee,
+macro_rules! impl_payment_obligation {
+    ($target:path) => {
+        impl From<(TokenBundleData, Address)> for $target {
+            fn from((bundle, payee): (TokenBundleData, Address)) -> Self {
+                let components = bundle.into_bundle_components();
+                Self {
+                    erc20Tokens: components.0,
+                    erc20Amounts: components.1,
+                    erc721Tokens: components.2,
+                    erc721TokenIds: components.3,
+                    erc1155Tokens: components.4,
+                    erc1155TokenIds: components.5,
+                    erc1155Amounts: components.6,
+                    payee,
+                }
+            }
         }
-    }
-}
-
-impl From<(TokenBundleData, Address)>
-    for contracts::erc20_barter_cross_token::TokenBundlePaymentObligation::StatementData
-{
-    fn from(data: (TokenBundleData, Address)) -> Self {
-        let data: contracts::token_bundle::TokenBundlePaymentObligation::StatementData =
-            data.into();
-        data.into()
-    }
+    };
 }
 
 impl From<(TokenBundleData, ArbiterData)>
     for contracts::token_bundle::TokenBundleEscrowObligation::StatementData
 {
-    fn from(data: (TokenBundleData, ArbiterData)) -> Self {
-        let (price, demand) = data;
+    fn from((bundle, arbiter_data): (TokenBundleData, ArbiterData)) -> Self {
+        let components = bundle.into_bundle_components();
 
-        contracts::token_bundle::TokenBundleEscrowObligation::StatementData {
-            erc20Tokens: price
-                .erc20s
-                .clone()
-                .into_iter()
-                .map(|erc20| erc20.address)
-                .collect(),
-            erc20Amounts: price.erc20s.into_iter().map(|erc20| erc20.value).collect(),
-            erc721Tokens: price
-                .erc721s
-                .clone()
-                .into_iter()
-                .map(|erc721| erc721.address)
-                .collect(),
-            erc721TokenIds: price.erc721s.into_iter().map(|erc721| erc721.id).collect(),
-            erc1155Tokens: price
-                .erc1155s
-                .clone()
-                .into_iter()
-                .map(|erc1155| erc1155.address)
-                .collect(),
-            erc1155TokenIds: price
-                .erc1155s
-                .clone()
-                .into_iter()
-                .map(|erc1155| erc1155.id)
-                .collect(),
-            erc1155Amounts: price
-                .erc1155s
-                .into_iter()
-                .map(|erc1155| erc1155.value)
-                .collect(),
-            arbiter: demand.arbiter,
-            demand: demand.demand,
+        Self {
+            erc20Tokens: components.0,
+            erc20Amounts: components.1,
+            erc721Tokens: components.2,
+            erc721TokenIds: components.3,
+            erc1155Tokens: components.4,
+            erc1155TokenIds: components.5,
+            erc1155Amounts: components.6,
+            arbiter: arbiter_data.arbiter,
+            demand: arbiter_data.demand,
         }
     }
 }
+
+impl_payment_obligation!(contracts::token_bundle::TokenBundlePaymentObligation::StatementData);
+impl_payment_obligation!(
+    contracts::erc20_barter_cross_token::TokenBundlePaymentObligation::StatementData
+);
+impl_payment_obligation!(
+    contracts::erc721_barter_cross_token::TokenBundlePaymentObligation::StatementData
+);
+impl_payment_obligation!(
+    contracts::erc1155_barter_cross_token::TokenBundlePaymentObligation::StatementData
+);
 
 pub enum ApprovalPurpose {
     Escrow,
