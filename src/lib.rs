4532dcf9d8f2b05cd1e51c87809abd6216db498c
疒奀 <yz@mmon.co>
2025-01-14 02:56:48 +0800

    take address config

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,11 @@ use alloy::{
     sol_types::SolEvent,
 };
 use clients::{
-    attestation::AttestationClient, erc1155::Erc1155Client, erc20::Erc20Client,
-    erc721::Erc721Client, token_bundle::TokenBundleClient,
+    attestation::{AttestationAddresses, AttestationClient},
+    erc1155::{Erc1155Addresses, Erc1155Client},
+    erc20::{Erc20Addresses, Erc20Client},
+    erc721::{Erc721Addresses, Erc721Client},
+    token_bundle::{TokenBundleAddresses, TokenBundleClient},
 };
 use futures_util::StreamExt;
 use sol_types::EscrowClaimed;
@@ -18,6 +21,15 @@ pub mod contracts;
 pub mod sol_types;
 pub mod types;
 pub mod utils;
+
+#[derive(Debug, Clone)]
+pub struct AddressConfig {
+    pub erc20_addresses: Option<Erc20Addresses>,
+    pub erc721_addresses: Option<Erc721Addresses>,
+    pub erc1155_addresses: Option<Erc1155Addresses>,
+    pub token_bundle_addresses: Option<TokenBundleAddresses>,
+    pub attestation_addresses: Option<AttestationAddresses>,
+}
 
 pub struct AlkahestClient {
     wallet_provider: WalletProvider,
@@ -34,19 +46,30 @@ impl AlkahestClient {
     pub fn new(
         private_key: impl ToString + Clone,
         rpc_url: impl ToString + Clone,
+        addresses: Option<AddressConfig>,
     ) -> eyre::Result<Self> {
         let wallet_provider = utils::get_wallet_provider(private_key.clone(), rpc_url.clone())?;
         let public_provider = utils::get_public_provider(rpc_url.clone())?;
+
+        macro_rules! make_client {
+            ($client:ident, $addresses:ident) => {
+                $client::new(
+                    private_key.clone(),
+                    rpc_url.clone(),
+                    addresses.clone().and_then(|a| a.$addresses),
+                )
+            };
+        }
 
         Ok(AlkahestClient {
             wallet_provider: wallet_provider.clone(),
             public_provider: public_provider.clone(),
 
-            erc20: Erc20Client::new(private_key.clone(), rpc_url.clone(), None)?,
-            erc721: Erc721Client::new(private_key.clone(), rpc_url.clone(), None)?,
-            erc1155: Erc1155Client::new(private_key.clone(), rpc_url.clone(), None)?,
-            token_bundle: TokenBundleClient::new(private_key.clone(), rpc_url.clone(), None)?,
-            attestation: AttestationClient::new(private_key.clone(), rpc_url.clone(), None)?,
+            erc20: make_client!(Erc20Client, erc20_addresses)?,
+            erc721: make_client!(Erc721Client, erc721_addresses)?,
+            erc1155: make_client!(Erc1155Client, erc1155_addresses)?,
+            token_bundle: make_client!(TokenBundleClient, token_bundle_addresses)?,
+            attestation: make_client!(AttestationClient, attestation_addresses)?,
         })
     }
 
