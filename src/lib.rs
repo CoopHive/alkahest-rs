afda1324861fd18c8e103ccd7e6631a8ad45835d
疒奀 <yz@mmon.co>
2025-01-16 16:59:25 +0800

    derive clone

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ pub struct AddressConfig {
     pub attestation_addresses: Option<AttestationAddresses>,
 }
 
+#[derive(Clone)]
 pub struct AlkahestClient {
     wallet_provider: WalletProvider,
     public_provider: PublicProvider,
