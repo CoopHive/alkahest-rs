d19332740a2d4eebe1310fffd84a70b52dc5c3ec
疒奀 <yz@mmon.co>
2025-02-25 01:05:46 +0800

    decode, encode, make statement

--- src/clients/string_obligation.rs
+++ src/clients/string_obligation.rs
@@ -1,9 +1,12 @@
 use alloy::{
-    primitives::{address, Address},
+    primitives::{address, Address, Bytes, FixedBytes},
+    rpc::types::TransactionReceipt,
     signers::local::PrivateKeySigner,
+    sol_types::SolValue as _,
 };
+use serde::de::DeserializeOwned;
 
-use crate::types::WalletProvider;
+use crate::{contracts, types::WalletProvider};
 
 #[derive(Debug, Clone)]
 pub struct StringObligationAddresses {
@@ -51,5 +54,74 @@ impl StringObligationClient {
             wallet_provider,
             addresses: addresses.unwrap_or_default(),
         })
+    }
+
+    pub fn decode(
+        statement_data: Bytes,
+    ) -> eyre::Result<contracts::StringObligation::StatementData> {
+        let statementdata =
+            contracts::StringObligation::StatementData::abi_decode(statement_data.as_ref(), true)?;
+        Ok(statementdata)
+    }
+
+    pub fn decode_json<T: DeserializeOwned>(statement_data: Bytes) -> eyre::Result<T> {
+        let decoded: T = serde_json::from_str(&Self::decode(statement_data)?.item)?;
+        Ok(decoded)
+    }
+
+    pub fn encode(statement_data: contracts::StringObligation::StatementData) -> Bytes {
+        return contracts::StringObligation::StatementData::abi_encode(&statement_data).into();
+    }
+
+    pub fn encode_json<T: serde::Serialize>(statement_data: T) -> eyre::Result<Bytes> {
+        let encoded = Self::encode(contracts::StringObligation::StatementData {
+            item: serde_json::to_string(&statement_data)?,
+        });
+        Ok(encoded)
+    }
+
+    pub async fn make_statement(
+        &self,
+        statement_data: contracts::StringObligation::StatementData,
+        ref_uid: Option<FixedBytes<32>>,
+    ) -> eyre::Result<TransactionReceipt> {
+        let contract =
+            contracts::StringObligation::new(self.addresses.obligation, &self.wallet_provider);
+
+        let receipt = contract
+            .makeStatement(
+                statement_data,
+                ref_uid.unwrap_or(FixedBytes::<32>::default()),
+            )
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
+    }
+
+    pub async fn make_statement_json<T: serde::Serialize>(
+        &self,
+        statement_data: T,
+        ref_uid: Option<FixedBytes<32>>,
+    ) -> eyre::Result<TransactionReceipt> {
+        let contract =
+            contracts::StringObligation::new(self.addresses.obligation, &self.wallet_provider);
+
+        let statement_data = contracts::StringObligation::StatementData {
+            item: serde_json::to_string(&statement_data)?,
+        };
+        let receipt = contract
+            .makeStatement(
+                statement_data,
+                ref_uid.unwrap_or(FixedBytes::<32>::default()),
+            )
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        Ok(receipt)
     }
 }
