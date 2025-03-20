5d92c803d93ca399aa0d1d12cbd02559bf6ddfe5
疒奀 <yz@mmon.co>
2025-03-20 13:44:37 +0800

    support base sepolia

--- src/addresses.rs
+++ src/addresses.rs
@@ -9,6 +9,50 @@ use crate::{
     AddressConfig,
 };
 
+pub const BASE_SEPOLIA_ADDRESSES: AddressConfig = AddressConfig {
+    arbiters_addresses: Some(ArbitersAddresses {
+        specific_attestation_arbiter: address!("0xdE5eCFC92E3da87865CD29C196aA5cebFdC4D9C6"),
+        trusted_party_arbiter: address!("0x3895398C46da88b75eE3ca3092F7714BEbE795a5"),
+        trivial_arbiter: address!("0x7D4bCD84901cEC903105564f63BE70432448B222"),
+        trusted_oracle_arbiter: address!("0x361E0950534F4a54A39F8C4f1f642C323f6e66B9"),
+    }),
+    string_obligation_addresses: Some(StringObligationAddresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        obligation: address!("0x4edEa259C8E014eeEd583D1a863e020190B21Db7"),
+    }),
+    erc20_addresses: Some(Erc20Addresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        barter_utils: address!("0x5C624f8FbbB377378cDfE8B627384A917FE839db"),
+        escrow_obligation: address!("0xFa76421cEe6aee41adc7f6a475b9Ef3776d500F0"),
+        payment_obligation: address!("0xE95d3931E15E4d96cE1d2Dd336DcEad35A708bdB"),
+    }),
+    erc721_addresses: Some(Erc721Addresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        barter_utils: address!("0x01414CC4a4c7b7fa9F551907ee89c867c7a74d29"),
+        escrow_obligation: address!("0xF3C3dC387e00FE76CCF7549634aa694D466de5AA"),
+        payment_obligation: address!("0x9DFe20Ded52D0F9e535F546f87d83B473DefC5B2"),
+    }),
+    erc1155_addresses: Some(Erc1155Addresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        barter_utils: address!("0x70a9Ce33CF0f7487A8a33B1447455047A90F3998"),
+        escrow_obligation: address!("0x4e7d759Df6204d901fb6FD82248FEa64f129bfa3"),
+        payment_obligation: address!("0x799048b0772381A095aa37305C1D85f26b8445C7"),
+    }),
+    token_bundle_addresses: Some(TokenBundleAddresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        barter_utils: address!("0xb03633005C763feAD6993541Cab2a10FA79828c1"),
+        escrow_obligation: address!("0xf63e97217f71C4cdbA643c8EFc9F152486560542"),
+        payment_obligation: address!("0xd192685E79F760fA769614d22916528254FD4937"),
+    }),
+    attestation_addresses: Some(AttestationAddresses {
+        eas: address!("0x4200000000000000000000000000000000000021"),
+        eas_schema_registry: address!("0x4200000000000000000000000000000000000020"),
+        barter_utils: address!("0xfFA2bf5Fc4270e9AFd20Aa2C87b3B100489DF97a"),
+        escrow_obligation: address!("0x021d28E9eBc935Bf21fe5Ff48cAAbE126Ed706aB"),
+        escrow_obligation_2: address!("0x5f177293F46d938316229A07E31bC65d64D58c9b"),
+    }),
+};
+
 pub const FILECOIN_CALIBRATION_ADDRESSES: AddressConfig = AddressConfig {
     arbiters_addresses: Some(ArbitersAddresses {
         specific_attestation_arbiter: address!("0x10788ba2c4c65d1e97bc6005436b61c2c2e51572"),
