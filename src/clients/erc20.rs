a2a8793c7b23eacf2f0dbe56d5564f563dbe9830
疒奀 <yz@mmon.co>
2025-03-25 21:22:36 +0800

    extra asserts and comments

--- src/clients/erc20.rs
+++ src/clients/erc20.rs
@@ -1055,6 +1055,7 @@ mod tests {
 
     use crate::{
         AlkahestClient,
+        fixtures::MockERC20Permit,
         types::{ApprovalPurpose, Erc20Data},
         utils::setup_test_environment,
     };
@@ -1063,30 +1064,63 @@ mod tests {
     //
     #[tokio::test]
     async fn test_buy_erc20_for_erc20() -> eyre::Result<()> {
+        // test setup
         let test = setup_test_environment().await?;
 
+        // give alice some erc20 tokens
+        let mock_erc20_a = MockERC20Permit::new(test.mock_addresses.erc20_a, &test.god_provider);
+        mock_erc20_a
+            .transfer(test.alice.address(), 100.try_into()?)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        // begin test
         let bid = Erc20Data {
             address: test.mock_addresses.erc20_a,
-            value: 10.try_into()?,
+            value: 100.try_into()?,
         };
         let ask = Erc20Data {
             address: test.mock_addresses.erc20_b,
-            value: 20.try_into()?,
+            value: 200.try_into()?,
         };
-        let expiration = 1000;
 
+        // alice approves tokens for escrow
         test.alice_client
             .erc20
             .approve(&bid, ApprovalPurpose::Escrow)
             .await?;
 
+        // alice makes escrow
         let receipt = test
             .alice_client
             .erc20
-            .buy_erc20_for_erc20(&bid, &ask, expiration)
+            .buy_erc20_for_erc20(&bid, &ask, 0)
             .await?;
-        let attested_event = AlkahestClient::get_attested_event(receipt)?;
 
+        let alice_balance = mock_erc20_a
+            .balanceOf(test.alice.address())
+            .call()
+            .await?
+            ._0;
+        let escrow_balance = mock_erc20_a
+            .balanceOf(
+                test.addresses
+                    .erc20_addresses
+                    .ok_or(eyre::eyre!("no erc20-related addresses"))?
+                    .escrow_obligation,
+            )
+            .call()
+            .await?
+            ._0;
+
+        // all tokens in escrow
+        assert_eq!(alice_balance, 0.try_into()?);
+        assert_eq!(escrow_balance, 100.try_into()?);
+
+        // escrow statement made
+        let attested_event = AlkahestClient::get_attested_event(receipt)?;
         assert_ne!(attested_event.uid, FixedBytes::<32>::default());
 
         Ok(())
